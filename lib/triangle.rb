d39610c1885fdf2d14298e293e95ec543fed621f
climb719 <mary.oldweiler@gmail.com>
2021-02-09 11:15:19 -0500

    commit tree b6999ba5246c9bd2e7acebc81ed29fbf633c9ba7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,28 +8,23 @@ class Triangle
   end
 
   def kind
-    # if firts_side + second_side < third_side || firts_side <= 0 || second_side <= 0 || third_side <= 0
-    #   begin
-    #     raise TriangleError
-    
-    #    rescue TriangleError => error
-    #      puts error.message
-    #    end
+    if  firts_side <= 0 || second_side <= 0 || third_side <= 0
+         raise TriangleError
+    elsif firts_side + second_side <= third_side || second_side + third_side <= firts_side || firts_side + third_side <= second_side
+        raise TriangleError
+       end
     if firts_side == second_side && second_side == third_side
       :equilateral
     elsif firts_side == second_side || second_side == third_side || firts_side == third_side
       :isosceles
     elsif firts_side != second_side && second_side != third_side && firts_side != third_side
       :scalene
-    # else  
-      
-      
     end
 
   end
 
   class TriangleError < StandardError
-    # "Illegal traingle"
+ 
   end
 
 
